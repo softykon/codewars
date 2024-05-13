0ac2fc535430b9a4efaabef1b108ebc15e9bdcab
softykon <dimakropochev077@gmail.com>
2024-05-13 08:43:06 +0600

    adfds

---
+++ Opposite number/main.go
@@ -0,0 +1,15 @@
+/* Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34 */
+
+package main
+
+func Opposite(value int) int {
+
+	return -value
+
+}
